dd00a9bcbdc6377361cb2c24b30ead23c2be5d53
Dmytro Chernovskyi <dimcher911@gmail.com>
2021-01-21 14:03:59 +0200

    Добавляет начальные файлы и настройки

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// slr - стрелочная с дефолтным экспортом
+// rafc - стрелочная с именованым экспортом
+
+const App = () => {
+    return (
+        <h1>Hello from React</h1>
+    );
+}
+
+export default App;
